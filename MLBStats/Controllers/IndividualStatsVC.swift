c557633c46ea6ae07be89fb0a36348a864ef0bc5
RobertWais <wais.robert@uwlax.edu>
2018-04-28 02:04:41 -0500

    Starting to pull data from cached SQLite

--- MLBStats/Controllers/IndividualStatsVC.swift
+++ MLBStats/Controllers/IndividualStatsVC.swift
@@ -14,7 +14,10 @@ class IndividualStatsVC: UIViewController,UIPickerViewDelegate,UIPickerViewDataS
     @IBOutlet var searchBtn: UIButton!
     @IBOutlet var collectionView: UICollectionView!
     @IBOutlet var picker: UIPickerView!
+    let queryStatementString = "SELECT PlayerID, FirstName, LastName, Position, JerseyNumber, Age, Height, Weight FROM Player"
+
     
+    var db: OpaquePointer? = nil
     var pickerDataSource = [["1", "2", "3", "4"],["A","B","C"],["a","b","c","d"]];
     var stats = ["SB","RBI","AVG","H","HR","R","AB"]
     var options = ["Most","Least","Top 5"]
@@ -29,35 +32,21 @@ class IndividualStatsVC: UIViewController,UIPickerViewDelegate,UIPickerViewDataS
         print("Option: \(optionsSeperator)")
         print("Team: \(teamOption)")
         //getvalues
-        
-        //if option, must be stat
-        if(true){
-        //team empty
-            //SELCT * FROM player WHERE ?(stat) ?(option)
-        }else if(true){
-        //team not empty
-            //Stat and option
-            //SELECT * FROM player where
-            
-            //just team
-            //
-            
-        }
+        selectNonTeamStats(type: "Most", attribute: "Hits")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Team")
         teams.sort()
         teams.insert("-", at:0)
-        
         collectionView.delegate = self
         collectionView.dataSource = self
         picker.delegate = self
         picker.dataSource = self
         picker.tintColor=UIColor.white
-    //et db = openDB()
-        // Do any additional setup after loading the view.
+        
+        //DB handling
+        db = openDB(path: prepareDatabaseFile())
     }
 
     override func didReceiveMemoryWarning() {
@@ -134,14 +123,74 @@ class IndividualStatsVC: UIViewController,UIPickerViewDelegate,UIPickerViewDataS
     
     
     
+    func query() {
+        var queryStatement: OpaquePointer? = nil
+        // 1
+        if sqlite3_prepare_v2(db, queryStatementString, -1, &queryStatement, nil) == SQLITE_OK {
+            // 2
+            var count=0;
+            while(sqlite3_step(queryStatement)) == SQLITE_ROW {
+                
+                let queryResultCol1 = sqlite3_column_int(queryStatement, 0)
+                let queryResultCol2 = sqlite3_column_text(queryStatement, 1)
+                let queryResultCol3 = sqlite3_column_text(queryStatement, 2)
+                let id = Int32(queryResultCol1)
+                let FirstName = String(cString: queryResultCol2!)
+                let lastName = String(cString:queryResultCol3!)
+                
+                print("Query Result:")
+                print("ID: \(id)")
+                print("FirstName: \(FirstName)")
+                print("LastName: \(lastName)")
+                count+=1
+                print("Count : \(count)")
+            }
+            print("Count \(count)")
+        } else {
+            print("SELECT statement could not be prepared")
+        }
+        
+        // 6
+        sqlite3_finalize(queryStatement)
+    }
     
     
+    func selectNonTeamStats(type: String, attribute: NSString){
+        print("Attempt")
+        var queryStatement: OpaquePointer? = nil
+        //case loop to decise string
+        var queryString: String?
+        queryString = "SELECT Player.FirstName, Player.LastName, Performance.Hits FROM Player JOIN Performance on Player.PlayerID = Performance.PlayerID ORDER BY (?)"
+        
+        if sqlite3_prepare_v2(db, queryString, -1, &queryStatement, nil) == SQLITE_OK {
+                //Entering Elements
+            sqlite3_bind_text(queryStatement, 1, attribute.utf8String, -1, nil)
+            
+            
+            while(sqlite3_step(queryStatement)) == SQLITE_ROW {
+                
+                //let queryResultCol1 = sqlite3_column_int(queryStatement, 0)
+                let queryResultCol1 = sqlite3_column_text(queryStatement, 0)
+                let queryResultCol2 = sqlite3_column_text(queryStatement, 1)
+                let queryResultCol3 = sqlite3_column_int(queryStatement, 2)
+                let hits = Int32(queryResultCol3)
+                let FirstName = String(cString: queryResultCol1!)
+                let LastName = String(cString: queryResultCol2!)
+                
+                print("FirstName: \(FirstName) \(LastName) \(hits)")
+                
+            }
+        }else {
+            print("SELECT statement could not be prepared")
+        }
+        
+        // 6
+        sqlite3_finalize(queryStatement)
+    }
     
+    //DATABASE STUFF
     
-    
-    
-    /*
-    func openDB()->OpaquePointer? {
+    func openDB(path: String) -> OpaquePointer? {
         var db: OpaquePointer? = nil
         print("Path: \(String(describing: path))")
         if sqlite3_open(path, &db) == SQLITE_OK {
@@ -152,15 +201,37 @@ class IndividualStatsVC: UIViewController,UIPickerViewDelegate,UIPickerViewDataS
         }
         return db
     }
- */
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    
+    func prepareDatabaseFile() -> String {
+        
+        print("In prepareDatabase")
+        let fileName: String = "MLBstatsFinal.db"
+        
+        let fileManager:FileManager = FileManager.default
+        let directory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        let documentUrl = directory.appendingPathComponent(fileName)
+        let bundleUrl = Bundle.main.url(forResource: "MLBstats", withExtension: "db", subdirectory: "Shared")
+        
+        // here check if file already exists on simulator
+        if fileManager.fileExists(atPath: (documentUrl.path)) {
+            print("document file exists!")
+            return documentUrl.path
+        }else if fileManager.fileExists(atPath: (bundleUrl?.path)!) {
+            print("document file does not exist, copy from bundle!")
+            do {
+                try fileManager.copyItem(at: bundleUrl!, to: documentUrl)
+            }catch let error {
+                print("Error---------------------")
+                print(error.localizedDescription)
+            }
+        }
+        
+        
+        
+        return documentUrl.path
     }
-    */
+ 
 
 }
