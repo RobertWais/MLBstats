d81c8051a587cb10e44453b40cbd0c2f0239f80e
RobertWais <wais.robert@uwlax.edu>
2018-04-30 16:35:37 -0500

    Changed table view and did searchbar programmatically

---
+++ MLBStats/TableViewCells/PlayerCellTVCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlayerCellTVCell.swift
+//  MLBStats
+//
+//  Created by Robert Wais on 4/30/18.
+//  Copyright © 2018 Robert Wais. All rights reserved.
+//
+
+import UIKit
+
+class PlayerCellTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
