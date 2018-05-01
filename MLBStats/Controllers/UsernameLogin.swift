2b3008d780df42f7f22c6e3d490618f15e5908c8
RobertWais <wais.robert@uwlax.edu>
2018-05-01 16:04:13 -0500

    Addede login view to setup users, changed name of view controllers

---
+++ MLBStats/Controllers/UsernameLogin.swift
@@ -0,0 +1,9 @@
+//
+//  UsernameLogin.swift
+//  MLBStats
+//
+//  Created by Robert Wais on 5/1/18.
+//  Copyright © 2018 Robert Wais. All rights reserved.
+//
+
+import Foundation
---
+++ MLBStats/Models/Team.swift
@@ -0,0 +1,9 @@
+//
+//  Team.swift
+//  MLBStats
+//
+//  Created by Robert Wais on 5/1/18.
+//  Copyright © 2018 Robert Wais. All rights reserved.
+//
+
+import Foundation
