2a20fc1546a9fba5c0b1466f11eb08cad7fa8a27
RobertWais <wais.robert@uwlax.edu>
2018-05-01 16:05:51 -0500

    Team class to work with StatsCell on TeamVC

--- MLBStats/Models/Team.swift
+++ MLBStats/Models/Team.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+struct Team{
+    
+    //
+    //Change Later, Structs, vs Classes
+    var _cityName: String?
+    var _teamName: String?
+    var _teamID: Int?
+    var _hits: Int?
+    var _runsBattedIn: Int?
+    var _battingAverage: Int?
+    var _stolenBases: Int?
+    var _homeRuns: Int?
+    var _runs: Int?
+    var _atBats: Int?
+    
+    
+    init(cityName: String, teamName: String, teamID: Int?, h: Int?, rbi: Int?, avg: Int?,sb: Int?, hr:  Int?, r: Int?, ab: Int?){
+        _cityName = cityName
+        _teamName = teamName
+        _teamID = teamID
+        _hits = h
+        _runsBattedIn = rbi
+        _battingAverage = avg
+        _stolenBases = sb
+        _homeRuns = hr
+        _runs=r
+        _atBats=ab
+    }
+    
+    
+}
