2987820ce44df0bf70f8ce6f7948c4a8b4b94cc7
RobertWais <wais.robert@uwlax.edu>
2018-05-01 22:25:34 -0500

    Added static class for user

---
+++ MLBStats/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  MLBStats
+//
+//  Created by Robert Wais on 5/1/18.
+//  Copyright © 2018 Robert Wais. All rights reserved.
+//
+
+import Foundation
+class User {
+    static let instance = User()
+    var username: String?
+    var userId: Int?
+    
+}
